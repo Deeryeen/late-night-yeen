2437ffd7c5a249d9069e55297a54075173d2bb10
Deeryeen <husky@deeryeen.com>
2021-08-10 02:55:51 +0200

    Laying the groundwork for the management API

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, Markup
+from flask import Flask, render_template, Markup, request
 from pathlib import Path
 from note import Note
+from datetime import datetime
+from yaml import load, Loader
+import json
 
 app = Flask(__name__, template_folder='templates/', static_folder='static/')
 
@@ -21,6 +24,15 @@ def note(note_name):
         return render_template('error.html', filename=note_name)
     note_object = Note(note_file)
     return render_template('note.html', name=note_object.name, description=note_object.description, contents=Markup(note_object.contents))
+
+# Managing the notes.
+@app.route('/manage/list', methods=['GET'])
+def manage_list():
+    notes = []
+    for note in notes_folder.rglob('*.md'):
+        notes.append(Note(note).metadata())
+
+    return json.dumps(notes)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- note.py
+++ note.py
@@ -1,9 +1,12 @@
 from markdown2 import markdown, Markdown
 from pathlib import Path
+from datetime import datetime
 
 class Note:
     name: str
     description: str
+    created: datetime
+    updated: datetime
     contents: Markdown
     filename: str
 
@@ -13,7 +16,12 @@ class Note:
         
         self.name = self.contents.metadata['name']
         self.description = self.contents.metadata['description']
+        # self.created = datetime.fromisoformat(self.contents.metadata['created'])
+        # self.updated = datetime.fromisoformat(self.contents.metadata['updated'])
         self.filename = filename
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
         return {'name':self.name, 'description':self.description, 'contents':self.contents, 'filename':self.filename.name}
+
+    def metadata(self) -> dict:
+        return {'name': self.name, 'description': self.description, 'filename': self.filename.name}
